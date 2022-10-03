9013cc8f14c61fb11a98b485d048819cdc2be1d7
Hamza Afzal <95589034+ArceusDev@users.noreply.github.com>
2022-10-03 15:16:55 -0700

    Multiple States added and bug removal out of range exception

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;  
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = state.GetNextStates();
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];  
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }*/
+
+        textComponent.text = state.GetStateStory();
+    }
+}
